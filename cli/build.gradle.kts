012c833c6c6020d243c3687acbc7bede7325aa6f
Vinicius Egidio <me@vinicius.io>
2024-02-12 13:42:29 +0100

    🔧 Simplified the CLI's build.gradle.kts

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -7,23 +7,15 @@ group = "io.vinicius.umd"
 version = "1.0.0"
 
 kotlin {
-    val hostOs = System.getenv("KTOS") ?: System.getProperty("os.name")
-    val isArm64 = System.getenv("KTARCH").equals("arm64", true) || System.getProperty("os.arch") == "aarch64"
-    val isMac = hostOs.contains("mac", true)
-    val isLinux = hostOs.contains("linux", true)
-    val isWindows = hostOs.contains("win", true)
+    val targets = listOf(
+        macosArm64(),
+        macosX64(),
+        linuxX64(),
+        mingwX64()
+    )
 
-    val nativeTarget = when {
-        isMac && isArm64 -> macosArm64()
-        isMac && !isArm64 -> macosX64()
-        isLinux && isArm64 -> linuxArm64()
-        isLinux && !isArm64 -> linuxX64()
-        isWindows -> mingwX64()
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
-        binaries {
+    targets.forEach {
+        it.binaries {
             executable {
                 entryPoint = "io.vinicius.umd.main"
                 baseName = "umd"
